0bc338677dabfc6c5ea83bd1ceda3767c876b560
DereWah <daviloca03@gmail.com>
2024-06-30 16:35:22 +0200

    fix: catch case in which the console is sending the command in CmdCounter.java fix: add config reload in CmdCounter.java fix: add check for args length

--- src/main/java/org/derewah/derecounter/commands/CmdCounter.java
+++ src/main/java/org/derewah/derecounter/commands/CmdCounter.java
@@ -1,9 +1,7 @@
 package org.derewah.derecounter.commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.derewah.derecounter.DereCounter;
 import org.derewah.derecounter.objects.CounterSet;
@@ -16,37 +14,44 @@ import java.util.List;
 public class CmdCounter implements CommandExecutor, TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player p = (Player) commandSender;
-        if(p.hasPermission("derecounter.admin")) {
-            if (args[0].equals("reload")) {
-                DereCounter.getInstance().loadLang();
-                p.sendMessage(Lang.PREFIX + Lang.PLUGIN_RELOADED.toString());
-                return true;
-            } else if (args[0].equals("set")) {
-                if (args.length == 2) {
-                    CounterSet cassaSet = new CounterSet(args[1]);
-                    p.getInventory().addItem(cassaSet.getItem());
-                    p.sendMessage(Lang.PREFIX.toString() + Lang.RECEIVED_SETCOUNTER.toString());
+        if(commandSender.hasPermission("derecounter.admin")) {
+            if(args.length > 0){
+                if (args[0].equals("reload")) {
+                    DereCounter.getInstance().loadLang();
+                    DereCounter.getInstance().reloadConfig();
+                    commandSender.sendMessage(Lang.PREFIX + Lang.PLUGIN_RELOADED.toString());
                     return true;
+                } else if (args[0].equals("set")) {
+                    if (args.length == 2 && commandSender instanceof Player) {
+                        Player p = (Player) commandSender;
+                        CounterSet cassaSet = new CounterSet(args[1]);
+                        p.getInventory().addItem(cassaSet.getItem());
+                        p.sendMessage(Lang.PREFIX.toString() + Lang.RECEIVED_SETCOUNTER.toString());
+                        return true;
+                    } else {
+                        commandSender.sendMessage(Lang.PREFIX.toString() + Lang.INVALID_COMMAND_ARG.toString());
+                        return true;
+                    }
                 } else {
-                    p.sendMessage(Lang.PREFIX.toString() + Lang.INVALID_COMMAND_ARG.toString());
+                    commandSender.sendMessage(Lang.PREFIX + Lang.CORRECT_USAGE.toString());
                     return true;
                 }
             } else {
-                p.sendMessage(Lang.PREFIX + Lang.CORRECT_USAGE.toString());
+                commandSender.sendMessage(Lang.PREFIX + Lang.CORRECT_USAGE.toString());
                 return true;
             }
         }else{
-            p.sendMessage(Lang.NO_PERMS.toString().replace("%permission%", "derecounter.admin"));
+            commandSender.sendMessage(Lang.NO_PERMS.toString().replace("%permission%", "derecounter.admin"));
             return true;
         }
     }
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
-        Player p = (Player) commandSender;
-        if(args.length == 1 && p.hasPermission("derecounter.admin")){
-            return Arrays.asList("reload", "set");
+        if(commandSender instanceof Player){
+            if(args.length == 1 && commandSender.hasPermission("derecounter.admin")){
+                return Arrays.asList("reload", "set");
+            }
         }
         return Arrays.asList();
     }
